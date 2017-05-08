cd31874aa32ee41dc620a9344d5a6cf92a052928
weili10 <lwei1993@gmail.com>
2017-05-08 20:57:08 +1000

    flask web framwork

---
+++ getGeoJSON.py
@@ -0,0 +1,41 @@
+from flask import Flask,request,make_response
+import urllib
+from geojson import Feature, Point, FeatureCollection
+import couchdb
+import json
+import geojson
+
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+    #var = urllib.request.urlopen("https://wanderdrone.appspot.com/").read()
+    #var = var.decode("utf-8")
+    
+    #print (type(var))
+
+    f = open("testGeoJson.geojson", encoding='utf-8')
+    geoJSON = geojson.load(f)
+
+    #rst = make_response(var)
+    rst = make_response(str(geoJSON))  
+    rst.headers['Access-Control-Allow-Origin'] = '*'
+    # a = {"geometry": {"type": "Point", "coordinates": [132.63686443893161, -27.190744312165926]}, "type": "Feature", "properties": {}}
+    # data = json.dumps(a)
+    # print (type(data))
+    print (rst)
+    return rst
+
+
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    name=request.form['yourname']
+    return "Welcome, " + name +'\n'
+
+
+if __name__ == '__main__':
+  app.run(
+        host="",
+        port=1337
+  )
